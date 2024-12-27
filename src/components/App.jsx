7e1da542580bd973115d424f37b98897491921f8
Danik2023 <danik.kyrteu2@gmail.com>
2024-12-27 16:24:38 +0200

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { Formik, Field, ErrorMessage } from 'formik';
 import { StyledForm, ErrorText } from './App.Styled';
 import * as Yup from 'yup';
+import { nanoid } from 'nanoid';
 
 const numbersSchema = Yup.object().shape({
   filter: '',
@@ -10,18 +11,24 @@ const numbersSchema = Yup.object().shape({
 });
 
 export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  addNumber = () => {};
   render() {
     return (
       <div>
         <Formik
           initialValues={{
-            contacts: [
-              { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-              { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-              { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-              { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-            ],
-            filter: '',
             name: '',
             number: '',
           }}
